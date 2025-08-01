93913d78de09f5e5be148788a51aca4ac3cfb610
Anthony Libardi <libardi42@gmail.com>
2025-08-01 17:01:28 -0400

    doing it as chunks and then pixels again

--- scr/terrain_generator.gd
+++ scr/terrain_generator.gd
@@ -7,6 +7,10 @@ var gen_thread := Thread.new()
 
 var source_image: Image
 var transparency_threshold: float
+var chunk_size: int
+var chunk_width: int
+var chunk_height: int
+var total_chunks: int
 var progress_callback: Callable
 var done_callback: Callable
 var ground_bitmap: BitMap = BitMap.new()
@@ -15,9 +19,13 @@ var polys: Array[PackedVector2Array]
 # Entry ============================================================================================
 
 @warning_ignore("shadowed_variable")
-func begin_generate(source_image: Image, transparency_threshold: float, progress_callback: Callable, done_callback: Callable):
+func begin_generate(source_image: Image, transparency_threshold: float, chunk_size: int, progress_callback: Callable, done_callback: Callable):
     self.source_image = source_image
     self.transparency_threshold = transparency_threshold
+    self.chunk_size = chunk_size
+    chunk_width = ceili(source_image.get_width() / float(chunk_size))
+    chunk_height = ceili(source_image.get_height() / float(chunk_size))
+    total_chunks = chunk_width * chunk_height
     self.progress_callback = progress_callback
     self.done_callback = done_callback
     gen_thread.start(generate)
